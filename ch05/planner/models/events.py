44336ef9fd921bc13dec91b637483b0a9e22c368
Lucas Vasconcelos <vasconcelos.contabeis@gmail.com>
2024-10-31 22:15:16 -0300

    Resolução de alguns exercícios do Capítulo 5

---
+++ ch05/planner/models/events.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class Event(BaseModel):
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: list[str]
+    location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.\
+                Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "lounch"],
+                "location": "Google Meet"
+            }
+        }
---
+++ ch05/planner/routes/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException, status
+from models.users import User, UserSingIn
+
+user_router = APIRouter(tags=["User"])
+
+users = {}
+
+@user_router.post("/singup")
+async def sing_nes_user(data: User) -> dict:
+    if data.email in users:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User with supplied username exists")
+    users[data.email] = data
+    return {"menssage": "User sucessfully registered!"}
+
+@user_router.post("/singin")
+async def sign_user_in(user: UserSingIn) -> dict:
+    if user.email not in users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist.")
+    if users[user.email].password != user.password:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Wrong credentials passed")
+    return {"message": "User signed in successfully."}
