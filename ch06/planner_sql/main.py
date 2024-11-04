58c584576e340fac12bf132924c258843bc277ed
Lucas Vasconcelos <vasconcelos.contabeis@gmail.com>
2024-11-04 16:54:40 -0300

    Resolução do exercício do Capítulo 5 para implementação de app com SQL

--- ch06/planner_sql/main.py
+++ ch06/planner_sql/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import RedirectResponse
 from contextlib import asynccontextmanager
 from database.connection import conn
 from routes.users import user_router
-from routes.events import even_router
+from routes.events import event_router
 
 import uvicorn
 
@@ -16,7 +16,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router, prefix="/user")
-app.include_router(even_router, prefix="/event")
+app.include_router(event_router, prefix="/event")
     
 
 @app.get("/")
