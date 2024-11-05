2520de16ff4c9e9d142dbaba20a3d4b9c6425ac1
Lucas Vasconcelos <vasconcelos.contabeis@gmail.com>
2024-11-04 21:37:08 -0300

    Resolução de mais alguma partes dos exercícios do Capítulo 5

---
+++ ch06/planner_nosql/models/users.py
@@ -0,0 +1,33 @@
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
+from models.events import Event
+
+class User(Document):
+    email: EmailStr
+    password: str
+    event: list[Link[Event]] | None = None
+
+    class Settings:
+        name = "users"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com.",
+                "username": "strong!!!",
+                "event": [],
+            }
+        }
+
+class UserSingIn(BaseModel):
+    email: EmailStr
+    password: str
+    
+    class Config:
+        schema_extra = {            
+                "example": {
+                    "email": "fastapi@packt.com.",
+                    "username": "strong!!!",
+                    "event": [],
+                }
+            }
